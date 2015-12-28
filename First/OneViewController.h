0d77b64ffd112c1c51a76b73bf24ca2320237050
JCHSS <136691850@qq.com>
2015-12-28 17:54:20 +0800

    qq

---
+++ First/OneViewController.h
@@ -0,0 +1,13 @@
+//
+//  OneViewController.h
+//  First
+//
+//  Created by MS on 15-12-28.
+//  Copyright (c) 2015年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OneViewController : UIViewController
+
+@end
---
+++ First/OneViewController.m
@@ -0,0 +1,37 @@
+//
+//  OneViewController.m
+//  First
+//
+//  Created by MS on 15-12-28.
+//  Copyright (c) 2015年 MS. All rights reserved.
+//
+
+#import "OneViewController.h"
+
+@interface OneViewController ()
+
+@end
+
+@implementation OneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
