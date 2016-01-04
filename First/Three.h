963d12834cd7c26aa78fa7967a962bc98ed3fe79
JCHSS <136691850@qq.com>
2016-01-04 15:12:20 +0800

    试试

---
+++ First/Three.h
@@ -0,0 +1,13 @@
+//
+//  Three.h
+//  First
+//
+//  Created by MS on 16-1-4.
+//  Copyright (c) 2016年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Three : UIViewController
+
+@end
---
+++ First/Three.m
@@ -0,0 +1,13 @@
+//
+//  Three.m
+//  First
+//
+//  Created by MS on 16-1-4.
+//  Copyright (c) 2016年 MS. All rights reserved.
+//
+
+#import "Three.h"
+
+@implementation Three
+
+@end
